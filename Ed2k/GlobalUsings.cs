57dc3cf6313ad03767f8599130f66bdb701cb1d2
Jan Klass <kissaki@posteo.de>
2024-03-06 17:50:34 +0100

    misc(proj): Use implicit instead of global usings

--- Ed2k/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using KCode.MD4Hash;
-global using System;
-global using System.Buffers;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Runtime.InteropServices;
-global using System.Threading.Tasks;
