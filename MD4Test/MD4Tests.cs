9f8ab2bb67cc3290365437ef999daae2991e5dcd
Jan Klass <kissaki@posteo.de>
2024-06-05 21:40:43 +0200

    test: Switch from xUnit to MSTest
    Previously behind higher paid tiers, [MSTest was open-sourced in 2017](https://devblogs.microsoft.com/devops/mstest-v2-is-open-source/).
    
    MSTest and the surrounding test tooling (SDK, runner, etc) [are actively being improved](https://devblogs.microsoft.com/dotnet/tag/mstest/).

--- MD4Test/MD4Tests.cs
+++ MD4Test/MD4Tests.cs
@@ -3,29 +3,29 @@ using System.Text;
 
 namespace MD4Test;
 
+[TestClass]
 public class MD4Tests
 {
-    [Fact]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Assertions", "xUnit2000:Constants and literals should be the expected argument", Justification = "<Pending>")]
+    [TestMethod]
     public void TestConstants()
     {
         // MD4 is bit based - but we implement byte based
         // 512 bit = 64 byte
-        Assert.Equal(64, 512 / 8);
-        Assert.Equal(64, MD4Internals.InputBlockLength);
+        Assert.AreEqual(64, 512 / 8);
+        Assert.AreEqual(64, MD4Internals.InputBlockLength);
 
         // This r2const test fails. Verify where this formula came from. Give nthat ed2k works fine MD4 and the r2const can't be wrong, right?
-        //Assert.Equal((uint)Math.Sqrt(2) * Math.Pow(2, 30), MD4Internals.r2const);
-        //Assert.Equal((uint)Math.Sqrt(3) * Math.Pow(2, 30), MD4Internals.r3const);
+        //Assert.AreEqual((uint)Math.Sqrt(2) * Math.Pow(2, 30), MD4Internals.r2const);
+        //Assert.AreEqual((uint)Math.Sqrt(3) * Math.Pow(2, 30), MD4Internals.r3const);
     }
 
     /// <summary>
     /// The initial values are defined in RFC 1186
     /// </summary>
-    [Fact]
+    [TestMethod]
     public void TestInitialDigestValues()
     {
-        Assert.Equal(new byte[] {
+        CollectionAssert.AreEqual(new byte[] {
             0x01, 0x23, 0x45, 0x67,
             0x89, 0xab, 0xcd, 0xef,
             0xfe, 0xdc, 0xba, 0x98,
@@ -33,29 +33,29 @@ public class MD4Tests
         }, MD4Internals.InitialABCD.ToArray());
     }
 
-    [Theory]
-    [InlineData(64, 0)]
-    [InlineData(64, 1)]
-    [InlineData(64, 2)]
-    [InlineData(64, 64 - 10)]
-    [InlineData(64, 64 - 9)]
-    [InlineData(128, 64 - 8)]
-    [InlineData(128, 64 - 7)]
-    [InlineData(128, 64 - 6)]
-    [InlineData(128, 64 - 5)]
-    [InlineData(128, 64 - 4)]
-    [InlineData(128, 64 - 3)]
-    [InlineData(128, 64 - 2)]
-    [InlineData(128, 64 - 1)]
-    [InlineData(128, 64)]
-    [InlineData(128, 64 + 1)]
-    [InlineData(128, 128 - 10)]
-    [InlineData(128, 128 - 9)]
-    [InlineData(192, 128 - 8)]
-    [InlineData(192, 128 - 7)]
+    [TestMethod]
+    [DataRow(64, 0)]
+    [DataRow(64, 1)]
+    [DataRow(64, 2)]
+    [DataRow(64, 64 - 10)]
+    [DataRow(64, 64 - 9)]
+    [DataRow(128, 64 - 8)]
+    [DataRow(128, 64 - 7)]
+    [DataRow(128, 64 - 6)]
+    [DataRow(128, 64 - 5)]
+    [DataRow(128, 64 - 4)]
+    [DataRow(128, 64 - 3)]
+    [DataRow(128, 64 - 2)]
+    [DataRow(128, 64 - 1)]
+    [DataRow(128, 64)]
+    [DataRow(128, 64 + 1)]
+    [DataRow(128, 128 - 10)]
+    [DataRow(128, 128 - 9)]
+    [DataRow(192, 128 - 8)]
+    [DataRow(192, 128 - 7)]
     public void TestDetermineLength1(int e, int len)
     {
-        Assert.Equal(e, MD4Internals.DetermineLength(len));
+        Assert.AreEqual(e, MD4Internals.DetermineLength(len));
     }
 
     /// <summary>
@@ -66,7 +66,7 @@ public class MD4Tests
     /// <para>The input is appended with at least a 1 bit, so 0b1000_0000.</para>
     /// <para>In MD4 the input is padded to {*:input B:0b1000_000 ... 4B:input-len } - that is, it is appended with bit padding + input length (64 bit = 8 Byte) to a multiple of 512 bit = 64 Byte</para>
     ///  </remarks>
-    [Fact]
+    [TestMethod]
     public void TestDetermineLength2()
     {
         var minPad = 9;
@@ -75,32 +75,32 @@ public class MD4Tests
         var gate1Split = gate1Value - minPad;
         for (int i = 0; i < gate1Split; ++i)
         {
-            Assert.Equal(gate1Value, MD4Internals.DetermineLength(i));
+            Assert.AreEqual(gate1Value, MD4Internals.DetermineLength(i));
         }
 
         var gate2Value = 128;
         var gate2Split = gate2Value - minPad;
         for (int i = gate1Split + 1; i < gate2Split; ++i)
         {
-            Assert.Equal(gate2Value, MD4Internals.DetermineLength(i));
+            Assert.AreEqual(gate2Value, MD4Internals.DetermineLength(i));
         }
 
         var gate3Value = 192;
         var gate3 = gate3Value - minPad;
         for (int i = gate2Split + 1; i < gate3; ++i)
         {
-            Assert.Equal(gate3Value, MD4Internals.DetermineLength(i));
+            Assert.AreEqual(gate3Value, MD4Internals.DetermineLength(i));
         }
 
         var gate4Value = 256;
         var gate4Split = gate4Value - minPad;
         for (int i = gate3 + 1; i < gate4Split; ++i)
         {
-            Assert.Equal(gate4Value, MD4Internals.DetermineLength(i));
+            Assert.AreEqual(gate4Value, MD4Internals.DetermineLength(i));
         }
     }
 
-    [Fact]
+    [TestMethod]
     public void TestPadEmpty()
     {
         var input = Array.Empty<byte>();
@@ -111,11 +111,11 @@ public class MD4Tests
 
         var actual = MD4Internals.PadInput(input);
 
-        Assert.Equal(64, actual.Length);
-        Assert.Equal<byte>(expected, actual.ToArray());
+        Assert.AreEqual(64, actual.Length);
+        CollectionAssert.AreEqual(expected, actual.ToArray());
     }
 
-    [Fact]
+    [TestMethod]
     public void TestPad1Byte()
     {
         var input = new byte[] { 0b1010_0101 };
@@ -127,11 +127,11 @@ public class MD4Tests
 
         var actual = MD4Internals.PadInput(input);
 
-        Assert.Equal(64, actual.Length);
-        Assert.Equal<byte>(expected, actual.ToArray());
+        Assert.AreEqual(64, actual.Length);
+        CollectionAssert.AreEqual(expected, actual.ToArray());
     }
 
-    [Fact]
+    [TestMethod]
     public void TestPad2Byte()
     {
         var input = new byte[] { 0b0110_1001, 0b1111_0000, };
@@ -144,29 +144,29 @@ public class MD4Tests
 
         var actual = MD4Internals.PadInput(input);
 
-        Assert.Equal(64, actual.Length);
-        Assert.Equal<byte>(expected, actual.ToArray());
+        Assert.AreEqual(64, actual.Length);
+        CollectionAssert.AreEqual(expected, actual.ToArray());
     }
 
-    [Fact]
+    [TestMethod]
     public void TestEmptyData()
     {
         var md4 = new MD4([]);
 
-        Assert.Equal("31d6cfe0d16ae931b73c59d7e0c089c0", md4.DigestString);
+        Assert.AreEqual("31d6cfe0d16ae931b73c59d7e0c089c0", md4.DigestString);
     }
 
     // Test cases from RFC 1186 page 16/17 - MDtestsuite() MDstring(s) MDprint(MDp)
-    [Theory]
-    [InlineData("31d6cfe0d16ae931b73c59d7e0c089c0", "")]
-    [InlineData("bde52cb31de33e46245e05fbdbd6fb24", "a")]
-    [InlineData("a448017aaf21d8525fc10ae87aa6729d", "abc")]
-    [InlineData("d9130a8164549fe818874806e1c7014b", "message digest")]
-    [InlineData("043f8582f241db351ce627e153e7f0e4", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")]
-    [InlineData("e33b4ddc9c38f2199c3e7b164fcc0536", "12345678901234567890123456789012345678901234567890123456789012345678901234567890")]
+    [TestMethod]
+    [DataRow("31d6cfe0d16ae931b73c59d7e0c089c0", "")]
+    [DataRow("bde52cb31de33e46245e05fbdbd6fb24", "a")]
+    [DataRow("a448017aaf21d8525fc10ae87aa6729d", "abc")]
+    [DataRow("d9130a8164549fe818874806e1c7014b", "message digest")]
+    [DataRow("043f8582f241db351ce627e153e7f0e4", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")]
+    [DataRow("e33b4ddc9c38f2199c3e7b164fcc0536", "12345678901234567890123456789012345678901234567890123456789012345678901234567890")]
     public void Test(string expectedDigest, string text)
     {
         var md4 = new MD4(Encoding.ASCII.GetBytes(text).AsSpan());
-        Assert.Equal(expectedDigest, md4.DigestString);
+        Assert.AreEqual(expectedDigest, md4.DigestString);
     }
 }
